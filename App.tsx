8b2375d011682aa78a4e31bf591dd19223e0a213
Julia Lojek <julialojek@gmail.com>
2020-08-07 12:08:37 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello TypeScript!</Text>
+      <Text>Hello React Native!</Text>
+      <Text>Good to work with both of you again</Text>
       <StatusBar style="auto" />
     </View>
   );
