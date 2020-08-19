75e18d2d876234ee370cd072d1039fa3a215098e
Julia Lojek <julialojek@gmail.com>
2020-08-19 12:28:47 +0200

    change color button

--- screens/Project.tsx
+++ screens/Project.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { StackScreenProps } from "@react-navigation/stack";
 import { StackParamList } from "../navigation/StackNav";
@@ -36,12 +31,19 @@ const Project: React.FC<ProjectProps> = ({ navigation, route }) => {
       />
       <View style={styles.colorWrapper}>
         <Text style={styles.bigText}>kolor:</Text>
-        <TouchableOpacity
+        <View
           style={{ ...styles.colorBox, backgroundColor: currentColor }}
+        ></View>
+        <TouchableOpacity
+          style={styles.changeColorButton}
           onPress={() => setIsModalOpen(true)}
         >
           <Text style={styles.smallText}>zmień</Text>
-          <Ionicons name="ios-color-palette" size={26} style={styles.editIcon} />
+          <Ionicons
+            name="ios-color-palette"
+            size={22}
+            style={styles.editIcon}
+          />
         </TouchableOpacity>
       </View>
     </View>
@@ -59,23 +61,26 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   smallText: {
-    fontSize: 18
+    fontSize: 18,
   },
   colorBox: {
-    width: "60%",
-    // height: 24,
-    // borderColor: "#000",
-    // borderWidth: 1,
-    elevation: 4,
+    width: "40%",
+    height: 24,
+    borderColor: "#000",
+    borderWidth: 1,
+  },
+  changeColorButton: {
     flexDirection: "row",
+    elevation: 4,
     justifyContent: "flex-end",
     alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    backgroundColor: "pink",
   },
   editIcon: {
     paddingLeft: 10,
-  }
+  },
 });
 
 export default Project;
